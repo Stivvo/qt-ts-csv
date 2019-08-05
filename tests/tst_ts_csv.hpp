83eea6cbbc73949f1869b30d57b167e3868c0d67
Federico Guerinoni <guerinoni@micro-systems.it>
2019-08-05 22:56:47 +0200

    -s
    Refactor test csv ts
    
    Signed-off-by: Federico Guerinoni <guerinoni@micro-systems.it>

--- tests/tst_ts_csv.hpp
+++ tests/tst_ts_csv.hpp
@@ -12,6 +12,9 @@ class tst_TsCsv : public testing::Test
   public:
     std::vector<std::string> docs;
 
+    bool cmp_file(const std::string &in, const std::string &out,
+                  const std::string &expected);
+
   protected:
     void TearDown() override
     {
@@ -23,68 +26,64 @@ class tst_TsCsv : public testing::Test
 
 TEST_F(tst_TsCsv, conversion)
 {
-    const auto output = "\"context\"|\"source\"|\"translation\"|\"location\"|"
-                        "\"version\"|\"language\"\n"
-                        "\"AddNewForm\"|\"Cottura Manuale\"|\"Manual "
-                        "Cooking\"|\"../../QML/OggettiEditDash/AddNewForm.qml "
-                        "- 21\"|\"2.1\"|\"en_GB\"\n";
+    const auto exp = "\"context\"|\"source\"|\"translation\"|\"location\"|"
+                     "\"version\"|\"language\"\n"
+                     "\"AddNewForm\"|\"Cottura Manuale\"|\"Manual "
+                     "Cooking\"|\"../../QML/OggettiEditDash/AddNewForm.qml "
+                     "- 21\"|\"2.1\"|\"en_GB\"\n";
 
-    auto doc = Path().get_files_basename() + "csv_ts/r1.csv";
-    docs.emplace_back(doc);
-    Ts2Csv().convert(Path().get_files_basename() + "csv_ts/t1.ts", doc.c_str());
-
-    EXPECT_EQ(Reader().read(std::move(doc)), output);
+    EXPECT_TRUE(cmp_file("csv_ts/r1.csv", "csv_ts/t1.ts", exp));
 }
 
 TEST_F(tst_TsCsv, multirow)
 {
-    const auto output =
+    const auto exp =
         "\"context\"|\"source\"|\"translation\"|\"location\"|\"version\"|"
         "\"language\"\n"
         "\"Connettivita\"|\"Impostazioni\r\n    Wi-fi\"|\"WI-fi\r\n"
         "    settings\"|\"../../QML/OggettiSettings/Connettivita.qml - "
         "66\"|\"2.1\"|\"en_GB\"\n";
 
-    auto doc = Path().get_files_basename() + "csv_ts/r2.csv";
-    docs.emplace_back(doc);
-    Ts2Csv().convert(Path().get_files_basename() + "csv_ts/t2.ts", doc.c_str());
-
-    EXPECT_EQ(Reader().read(std::move(doc)), output);
+    EXPECT_TRUE(cmp_file("csv_ts/r2.csv", "csv_ts/t2.ts", exp));
 }
 
-// TEST_F(test_ts_csv, typeVanishedAndObsolete)
-//{
-//    const auto output =
-//    "\"context\"|\"source\"|\"translation\"|\"location\"|\"version\"|\"language\"\n";
+TEST_F(tst_TsCsv, typeVanishedAndObsolete)
+{
+    const auto exp = "\"context\"|\"source\"|\"translation\"|\"location\"|"
+                     "\"version\"|\"language\"\n";
 
-//    n_doc = "../../qt-ts-csv/tests/files/csv_ts/r3.csv";
-//    Ts2Csv().convert("../../qt-ts-csv/tests/files/csv_ts/t3.ts", n_doc);
+    EXPECT_TRUE(cmp_file("csv_ts/r3.csv", "csv_ts/t3.ts", exp));
+}
 
-//    EXPECT_EQ(Reader().read(n_doc), output);
-//}
+TEST_F(tst_TsCsv, dontDeleteUnfinished)
+{
+    const auto exp = "\"context\"|\"source\"|\"translation\"|\"location\"|"
+                     "\"version\"|\"language\"\n"
+                     "\"ProgrammaSettmodel\"|\"h\"|\"h\"|\"../../../Ricette/"
+                     "programmasettmodel.cpp - 687\"|\"2.1\"|\"en_US\"\n"
+                     "\"ProgrammaSettmodel\"|\"g\"|\"\"|\"../../../Ricette/"
+                     "programmasettmodel.cpp - 655\"|\"\"|\"\"\n";
 
-// TEST_F(test_ts_csv, dontDeleteUnfinished)
-//{
+    EXPECT_TRUE(cmp_file("csv_ts/r5.csv", "csv_ts/t5.ts", exp));
+}
 
-//    const auto output =
-//    "\"context\"|\"source\"|\"translation\"|\"location\"|\"version\"|\"language\"\n"
-//                        "\"ProgrammaSettmodel\"|\"h\"|\"h\"|\"../../../Ricette/programmasettmodel.cpp
-//                        - 687\"|\"2.1\"|\"en_US\"\n"
-//                        "\"ProgrammaSettmodel\"|\"g\"|\"\"|\"../../../Ricette/programmasettmodel.cpp
-//                        - 655\"|\"\"|\"\"\n";
+TEST_F(tst_TsCsv, completeConversion)
+{
+    auto doc          = Path().get_files_basename() + "csv_ts/r4.csv";
+    auto file_compare = Path().get_files_basename() + "csv_ts/tc4.csv";
+    docs.emplace_back(doc);
 
-//    n_doc = "../../qt-ts-csv/tests/files/csv_ts/r5.csv";
-//    Ts2Csv().convert("../../qt-ts-csv/tests/files/csv_ts/t5.ts", n_doc);
+    Ts2Csv().convert(Path().get_files_basename() + "csv_ts/t4.ts", doc.c_str());
 
-//    EXPECT_EQ(Reader().read(n_doc), output);
-//}
+    EXPECT_EQ(Reader().read(std::move(doc)),
+              Reader().read(std::move(file_compare)));
+}
 
-// TEST_F(test_ts_csv, completeConversion)
-//{
-//    n_doc = "../../qt-ts-csv/tests/files/r4.csv";
-//    const auto file_compare = "../../qt-ts-csv/tests/files/csv_ts/tc4.csv";
-
-//    Ts2Csv().convert("../../qt-ts-csv/tests/files/csv_ts/t4.ts", n_doc);
-
-//    EXPECT_EQ(Reader().read(n_doc), Reader().read(file_compare));
-//}
+bool tst_TsCsv::cmp_file(const std::string &in, const std::string &out,
+                         const std::string &expected)
+{
+    auto doc = Path().get_files_basename() + in;
+    docs.emplace_back(doc);
+    Ts2Csv().convert(Path().get_files_basename() + out, doc.c_str());
+    return Reader().read(std::move(doc)) == expected;
+}
