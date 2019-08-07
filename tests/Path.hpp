b9164d51810004cbe105a8cfaca85dbb6dce021b
Stivvo <entattis15@itisvinci.com>
2019-08-07 11:28:58 +0200

    Debug.hpp added
    Debug::file_string_eq does the same things of Path::debug

--- tests/Path.hpp
+++ tests/Path.hpp
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <Reader.hpp>
 #include <algorithm>
 #include <experimental/filesystem>
 #include <iostream>
@@ -18,29 +17,5 @@ struct Path {
 
         str.erase(it, str.size());
         return (str + "qt-ts-csv/tests/files/");
-    }
-    bool debug(std::string &doc, const std::string &expected) const noexcept
-    {
-        std::string docReaded = Reader().read(std::move(doc));
-
-        // debug
-        std::string diffs = "";
-        int j             = 0;
-        for (int i = 0; i < expected.size(); ++i) {
-            if (expected[i] == docReaded[j])
-                ++j;
-            else
-                diffs += expected[i];
-        }
-        std::cout << "docReaded: " << std::endl << docReaded << std::endl;
-        std::cout << "expected: " << std::endl << expected << std::endl;
-
-        std::cout << "docReaded size: " << docReaded.size() << std::endl;
-        std::cout << "expected size: " << expected.size() << std::endl;
-        std::cout << "j: " << j << ", diffs.size: " << diffs.size() << std::endl
-                  << std::endl;
-        // end debug
-
-        return docReaded == expected;
     }
 };
--- tests/tests.pro
+++ tests/tests.pro
@@ -12,6 +12,7 @@ include($$PWD/../src/src.pri)
 
 HEADERS += \
     $$PWD/Path.hpp \
+    $$PWD/Debug.hpp \
     $$PWD/tst_ts_csv.hpp \
     $$PWD/tst_csv_ts.hpp \
     $$PWD/tst_ts_xlsx.hpp \
