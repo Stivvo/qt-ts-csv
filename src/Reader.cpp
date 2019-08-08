c639ca6edc9198bcb155927243b71c18895e58c2
Stivvo <entattis15@itisvinci.com>
2019-08-08 08:26:45 +0200

    tst_ts_xlsx still not working

--- src/Reader.cpp
+++ src/Reader.cpp
@@ -1,13 +1,11 @@
 #include "Reader.hpp"
 
-#include <QFile>
 #include <fstream>
 #include <iostream>
 #include <sstream>
 
 std::string Reader::read(std::string &&filename) const
 {
-    QFile f(QString::fromStdString(filename));
     std::ifstream ifs;
     ifs.open(filename);
     std::stringstream ss;
