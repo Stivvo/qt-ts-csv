fe2c109a72ee2e1cbfab0d84af2eaa59d9db62b0
Stivvo <entattis15@itisvinci.com>
2019-08-08 08:26:45 +0200

    tst_ts_xlsx still not working

--- tests/tst_ts_xlsx.hpp
+++ tests/tst_ts_xlsx.hpp
@@ -2,6 +2,7 @@
 
 #include "Path.hpp"
 #include "Ts2Xlsx.hpp"
+#include "XlsxParser.hpp"
 
 #include <gmock/gmock-matchers.h>
 #include <gtest/gtest.h>
@@ -31,9 +32,12 @@ TEST_F(test_ts_xlsx, conversion)
 bool test_ts_xlsx::cmp_file(const std::string &in, const std::string &out,
                             const std::string &expected)
 {
-    auto file_compare = Path().get_files_basename() + f + in;
-    auto doc          = Path().get_files_basename() + f + out;
+    auto doc = Path().get_files_basename() + f + out;
     docs.push_back(doc);
-    std::string file_expected = Path().get_files_basename() + f + expected;
-    Ts2Xlsx().convert(std::move(file_expected), std::move(doc.c_str()));
+
+    Ts2Xlsx().convert(std::move(Path().get_files_basename() + f + in),
+                      std::move(doc.c_str()));
+
+    return XlsxParser().parse(doc.c_str()) ==
+           XlsxParser().parse(Path().get_files_basename() + f + expected);
 }
