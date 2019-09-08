dfa501b69e78b4dd765fb2161092931cd288dd87
Federico Guerinoni <guerinoni@micro-systems.it>
2019-09-08 03:31:25 +0200

    Refactor TS=>XLSX

--- tests/tst_ts_xlsx.cpp
+++ tests/tst_ts_xlsx.cpp
@@ -7,12 +7,12 @@
 static bool cmp_file(const std::string &in, const std::string &out,
                      const std::string &exp)
 {
-    std::string f = TestHelper::absolute_path("ts_xlsx");
+    const auto f = TestHelper::absolute_path("ts_xlsx");
 
-    std::string fIn  = f + in;
-    std::string fOut = f + out;
-    std::string fExp = f + exp;
-    auto fOut1       = fOut;
+    auto fIn   = f + in;
+    auto fOut  = f + out;
+    auto fExp  = f + exp;
+    auto fOut1 = fOut;
 
     if (fOut.find("Saved", 0) == std::string::npos) {
         TestHelper::instance.docs.emplace_back(fOut);
@@ -27,18 +27,15 @@ static bool cmp_file(const std::string &in, const std::string &out,
 
 TEST_CASE("TS -> XLSX")
 {
-    //    if (TestHelper::ToRun("ts_xlsx")) {
-    //        SECTION("conversion")
-    //        {
-    //            CHECK(cmp_file("conversionIn.ts", "conversionOut.xlsx",
-    //                           "conversionIn.xlsx"));
-    //        }
+    SECTION("conversion")
+    {
+        CHECK(cmp_file("input_strange.ts", "output_strange.xlsx",
+                       "expected_strange.xlsx"));
+    }
 
-    //        SECTION("compare the saved output of conversion with the manual "
-    //                "expected file")
-    //        {
-    //            CHECK(cmp_file("conversionIn.ts", "conversionOutSaved.xlsx",
-    //                           "conversionIn.xlsx"));
-    //        }
-    //    }
+    SECTION("compare the saved output of conversion with the manual "
+            "expected file")
+    {
+        CHECK(cmp_file("input.ts", "output.xlsx", "expected.xlsx"));
+    }
 }
