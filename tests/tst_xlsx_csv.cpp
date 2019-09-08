7b2ec95596e55fa3be30e726d7f24526013ed29e
Federico Guerinoni <guerinoni@micro-systems.it>
2019-09-08 04:07:23 +0200

    Semi-Fix XLSX=>CSV

--- tests/tst_xlsx_csv.cpp
+++ tests/tst_xlsx_csv.cpp
@@ -1,26 +1,29 @@
 #include "TestHelper.hpp"
 
+#include <Reader.hpp>
 #include <Xlsx2csv.hpp>
 #include <catch.hpp>
 
 TEST_CASE("XLSX -> CSV")
 {
-    //    if (TestHelper::ToRun("xlsx_csv")) {
-    //        std::string f = TestHelper::fullPath("xlsx_csv");
-    //        SECTION("complete conversion")
-    //        {
-    //            std::string fOut = f + "out.csv";
-    //            std::string fIn  = f + "exp.xlsx";
-    //            std::string fExp = f + "exp.csv";
-    //            auto fOut1       = fOut;
+    std::string f = TestHelper::absolute_path("xlsx_csv");
+    SECTION("complete conversion")
+    {
+        std::string fOut = f + "out.csv";
+        std::string fIn  = f + "exp.xlsx";
+        std::string fExp = f + "input.csv";
+        auto fOut1       = fOut;
 
-    //            TestHelper::pushDocs(fOut);
-    //            Xlsx2Csv().convert(std::move(fIn), std::move(fOut));
+        TestHelper::instance.docs.emplace_back(fOut);
+        Xlsx2Csv().convert(std::move(fIn), std::move(fOut));
 
-    //            auto rOut = Reader().read(std::move(fOut1));
-    //            auto rExp = Reader().read(std::move(fExp));
+        auto rOut = Reader().read(std::move(fOut1));
+        auto rExp = Reader().read(std::move(fExp));
 
-    //            CHECK(rOut == rExp);
-    //        }
-    //    }
+        /*
+         * FIXME: this tests doesn't pass because it use the same files of
+         * CSV -> XLSX and there is a problem when open xlsx
+         */
+        CHECK(rOut == rExp);
+    }
 }
