a74b0f84dc585ef4dcfa9431db92725239e84cca
Federico Guerinoni <guerinoni@micro-systems.it>
2019-09-08 03:01:50 +0200

    Remove unused func
    Signed-off-by: Federico Guerinoni <guerinoni@micro-systems.it>

--- tests/tst_csv_xlsx.cpp
+++ tests/tst_csv_xlsx.cpp
@@ -4,29 +4,23 @@
 #include <XlsxParser.hpp>
 #include <catch.hpp>
 
-static bool cmp_file(const std::string &in, const std::string &out,
-                     const std::string &exp)
-{
-    const auto f = TestHelper::absolute_path("xlsx_csv");
-
-    auto fOut  = f + out;
-    auto fIn   = f + in;
-    auto fExp  = f + exp;
-    auto fOut1 = fOut;
-
-    TestHelper::instance.docs.emplace_back(fOut);
-    Csv2Xlsx().convert(std::move(fIn), std::move(fOut));
-
-    auto rOut = XlsxParser().parse(std::move(fOut1));
-    auto rExp = XlsxParser().parse(std::move(fExp));
-
-    return rOut == rExp;
-}
-
 TEST_CASE("CSV -> XLSX")
 {
     SECTION("conversion")
     {
-        CHECK(cmp_file("input.csv", "out.xlsx", "exp.xlsx"));
+        const auto f = TestHelper::absolute_path("xlsx_csv");
+
+        auto fOut  = f + "out.xlsx";
+        auto fIn   = f + "input.csv";
+        auto fExp  = f + "exp.xlsx";
+        auto fOut1 = fOut;
+
+        TestHelper::instance.docs.emplace_back(fOut);
+        Csv2Xlsx().convert(std::move(fIn), std::move(fOut));
+
+        auto rOut = XlsxParser().parse(std::move(fOut1));
+        auto rExp = XlsxParser().parse(std::move(fExp));
+
+        CHECK(rOut == rExp);
     }
 }
