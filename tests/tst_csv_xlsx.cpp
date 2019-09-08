e2be6a7c1c244f4401c6cafddd081cf90423c9fa
Federico Guerinoni <guerinoni@micro-systems.it>
2019-09-08 02:46:50 +0200

    Refactor CSV=XLSX

--- tests/tst_csv_xlsx.cpp
+++ tests/tst_csv_xlsx.cpp
@@ -7,17 +7,15 @@
 static bool cmp_file(const std::string &in, const std::string &out,
                      const std::string &exp)
 {
-    std::string f = TestHelper::absolute_path("xlsx_csv");
+    const auto f = TestHelper::absolute_path("xlsx_csv");
 
-    std::string fOut = f + out;
-    std::string fIn  = f + in;
-    std::string fExp = f + exp;
-    auto fOut1       = fOut;
+    auto fOut  = f + out;
+    auto fIn   = f + in;
+    auto fExp  = f + exp;
+    auto fOut1 = fOut;
 
-    if (fOut.find("Saved", 0) == std::string::npos) {
-        TestHelper::instance.docs.emplace_back(fOut);
-        Csv2Xlsx().convert(std::move(fIn), std::move(fOut));
-    }
+    TestHelper::instance.docs.emplace_back(fOut);
+    Csv2Xlsx().convert(std::move(fIn), std::move(fOut));
 
     auto rOut = XlsxParser().parse(std::move(fOut1));
     auto rExp = XlsxParser().parse(std::move(fExp));
@@ -27,16 +25,8 @@ static bool cmp_file(const std::string &in, const std::string &out,
 
 TEST_CASE("CSV -> XLSX")
 {
-    //    if (TestHelper::ToRun("csv_xlsx")) {
-    //        SECTION("conversion")
-    //        {
-    //            CHECK(cmp_file("exp.csv", "out.xlsx", "exp.xlsx"));
-    //        }
-
-    //        SECTION("compare the saved output of conversion with the manual "
-    //                "expected file")
-    //        {
-    //            CHECK(cmp_file("exp.csv", "outSaved.xlsx", "exp.xlsx"));
-    //        }
-    //    }
+    SECTION("conversion")
+    {
+        CHECK(cmp_file("input.csv", "out.xlsx", "exp.xlsx"));
+    }
 }
